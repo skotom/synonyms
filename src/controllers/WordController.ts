1a128b7097c08fa1ac2144b659fd98dcdca8203d
Tomislav Skoda <tskoda23@gmail.com>
2024-10-24 09:33:03 +0200

    Fix no transitive property on reverse order of add

--- src/controllers/WordController.ts
+++ src/controllers/WordController.ts
@@ -5,18 +5,39 @@ export class WordController {
   static async save(req: Request, res: Response) {
     const word = req.body.word;
 
-    const synonyms = [
-      ...new Set<string>(
-        req.body.synonyms.map((synonym: string) => synonym.toLowerCase())
-      ),
-    ];
+    // use list of groups and hashmap that maps each word to group
+    // that way we won't have more than 2 duplicates
+    // hashmap will contain each word once
+    // and list of groups will contain all words once
+    // this way we'll take up less memory
+
+    let newSynonyms = req.body.synonyms;
+
+    let synonyms =
+      req.session.synonyms && Object.keys(req.session.synonyms).includes(word)
+        ? req.session.synonyms[word]
+        : [];
+
+    if (req.session.synonyms) {
+      for (const syn of newSynonyms) {
+        if (Object.keys(req.session.synonyms).includes(syn))
+          newSynonyms = WordService.getUnique(
+            newSynonyms,
+            req.session.synonyms[syn]
+          );
+      }
+    }
+
+    synonyms = WordService.getUnique(newSynonyms, synonyms);
 
     let data: any = {};
 
-    for (const synonym of synonyms) {
-      WordService.createWordVertex(word, synonym, req);
-      WordService.createWordVertex(synonym, word, req);
+    for (const k of [...synonyms, word]) {
+      for (const v of [...synonyms, word].filter((val) => val != k)) {
+        WordService.createWordVertex(k, v, req);
+      }
     }
+
     // TODO(tome): if synonym is missing then we remove it...
     // check if there is some synonym that exists in synonyms but not in params
     // and remove it
@@ -36,14 +57,10 @@ export class WordController {
 
     const synonyms = req.session.synonyms;
 
-    if (synonyms && Object.keys(synonyms).includes(word)) {
+    if (synonyms && word != "") {
       data = Object.keys(synonyms)
         .filter((key) => key.startsWith(word))
         .map((key) => ({ word: key, synonyms: synonyms[key] }));
-      // data = Object.fromEntries(
-      //   Object.entries(synonyms).filter(([k, v]) => k.startsWith(word))
-      // );
-      // maybe it would be better if we return it as a list
     } else {
       data = {};
     }
--- src/services/WordService.ts
+++ src/services/WordService.ts
@@ -1,5 +1,14 @@
 import { Request } from "express";
 export default class WordService {
+  static getUnique(arr1, arr2) {
+    const res = [
+      ...new Set<string>(
+        [...arr1, ...arr2].map((val: string) => val.toLowerCase())
+      ),
+    ];
+
+    return res;
+  }
   static createWordVertex(key, value, req: Request) {
     if (!req.session.synonyms) {
       req.session.synonyms = {};
