23f12f3cbf8516e41889b1ef5fa8ddb11f183b92
Tomislav Skoda <tskoda23@gmail.com>
2024-10-27 09:03:51 +0100

    Rearrange dirs

---
+++ server/src/config/config.ts
@@ -0,0 +1,12 @@
+const env: string =
+  (process.env.ENVIRONMENT as string) || (process.env.NODE_ENV as string);
+
+const port = process.env.PORT;
+
+if (env === "development" && !port) throw new Error("PORT not defined in .env");
+
+export const config = {
+  env: env,
+  port: port,
+  secret: process.env.SECRET,
+};
---
+++ server/src/server.ts
@@ -0,0 +1,51 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import session from "express-session";
+import { v4 as uuidv4 } from "uuid";
+dotenv.config();
+
+import { config } from "./config/config";
+import router from "./routes/main";
+
+const app: Application = express();
+
+app.use(
+  session({
+    genid: function () {
+      return uuidv4();
+    },
+    secret: config.secret,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const whitelist = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "https://synonyms.synctunes.fun",
+];
+
+app.use(
+  cors({
+    credentials: true,
+    origin: function (origin: string | undefined, callback: any) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
+
+app.use("/", router);
+
+app.listen(config.port, () =>
+  console.log(`API listening on port ${config.port}`)
+);
