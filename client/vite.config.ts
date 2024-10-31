dd9c4585ceb330e0afc5d517019224def7f6292c
Tomislav Skoda <tskoda23@gmail.com>
2024-10-31 09:11:22 +0100

    Fix vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
-    // vite config
     plugins: [react()],
     server: {
-      port: env.VITE_PORT,
+      port: Number(env.VITE_PORT),
     },
   };
 });
