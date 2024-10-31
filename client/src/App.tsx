57c8f979ed52ff693b6b9171034f7943d62aeb50
Tomislav Skoda <tskoda23@gmail.com>
2024-10-31 11:42:05 +0100

    Change toast position

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -66,7 +66,7 @@ export default function App() {
         autoClose={1000}
         theme="dark"
         draggable={true}
-        position="bottom-right"
+        position="top-right"
         transition={Slide}
         hideProgressBar={true}
       />
